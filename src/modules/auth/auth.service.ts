1013238ef82d8ed7723f28bff1d1012495f811b2
AshileySabah <ashileysabrina2002@gmail.com>
2026-01-29 17:00:36 -0300

    feat: auth - login

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,19 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import * as crypto from 'crypto';
 import { SankhyaDBExplorerSPClient } from 'src/http-client/db-explorer-sp/db-explorer-sp.client';
 import { LoginRequest } from './dto/auth.dto';
-import { GatewayClient } from 'src/http-client/gateway/gateway.client';
 
 @Injectable()
 export class AuthService {
-  constructor(
-    private readonly dbExplorerClient: SankhyaDBExplorerSPClient,
-    private readonly gateway: GatewayClient,
-  ) {}
+  constructor(private readonly dbExplorerClient: SankhyaDBExplorerSPClient) {}
 
   async login(body: LoginRequest) {
-    const response = this.gateway.client.post('/login', {
-      headers: { Username: body.usuario, Password: body.senha },
-    });
-    return response;
+    const { usuario, senha } = body;
+
+    const senhaCriptografada = crypto
+      .createHash('sha1')
+      .update(senha)
+      .digest('hex')
+      .toUpperCase();
+
+    const sql = `
+    SELECT CODUSUARIO, USUARIO, NOME 
+
+    FROM TGUSUARIO 
+
+    WHERE USUARIO = ${usuario} 
+    AND SENHA = ${senhaCriptografada} 
+    AND ATIVO = 'S' 
+    `;
+
+    const result = await this.dbExplorerClient.executeQuery(sql);
+
+    if (!result || result.length === 0) {
+      throw new UnauthorizedException('Usuário ou senha inválidos');
+    }
+
+    const usuarioLogado = result[0];
+    return {
+      idUsuario: usuarioLogado.CODUSUARIO,
+      usuario: usuarioLogado.USUARIO,
+      nome: usuarioLogado.NOME,
+    };
   }
 }
