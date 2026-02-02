31963d078b2a0c40ede31e2603014f68e4905d19
AshileySabah <ashileysabrina2002@gmail.com>
2026-02-02 15:54:57 -0300

    feat: login response

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { LoginRequest } from 'src/modules/auth/dto/auth.dto';
+import { HttpException, Injectable } from '@nestjs/common';
 import { SankhyaMobileLoginSPClient } from 'src/http-client/mobile-login-sp/mobile-login-sp.client';
+import { LoginRequest } from 'src/modules/auth/dto/auth.dto';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +8,18 @@ export class AuthService {
 
   async login(body: LoginRequest) {
     const response = await this.mobileLoginClient.login(body);
-    return response;
+
+    if (response.status === '0') {
+      throw new HttpException({ message: 'Usuário ou senha inválidos' }, 401);
+    }
+
+    if (response.status === '1') {
+      return {
+        callID: response.responseBody.callID.$,
+        jsessionid: response.responseBody.jsessionid.$,
+        kID: response.responseBody.kID.$,
+        idUsuario: response.responseBody.idusu.$,
+      };
+    }
   }
 }
