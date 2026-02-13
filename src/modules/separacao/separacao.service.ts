5487db7d5fd9fb613c61c1df9cfa4815b5069c71
AshileySabah <ashileysabrina2002@gmail.com>
2026-02-13 14:57:28 -0300

    feat: remove datase-sp

--- src/modules/separacao/separacao.service.ts
+++ src/modules/separacao/separacao.service.ts
@@ -52,27 +52,31 @@ export class SeparacaoService {
       WHERE CAB.NUNOTA = ${numeroUnico}
     `);
 
-    const aaa = {
-      serviceName: 'DatasetSP.save',
-      requestBody: {
-        entityName: 'CabecalhoConferencia',
-        standAlone: false,
-        fields: ['CODUSUCONF', 'NUCONF', 'QTDVOL', 'STATUS'],
-        records: [
-          {
-            pk: {
-              NUNOTAORIG: numeroUnico,
-            },
-            values: {
-              '0': idUsuario,
-              '1': numeroConferencia,
-              '2': 0,
-              '3': 'A',
-            },
-          },
-        ],
-      },
-    };
+    await this.dbExplorerClient.executeQuery(`
+    INSERT INTO TGFCON2 ( 
+    CODUSUCONF, 
+    DHFINCONF, 
+    DHINICONF, 
+    NUCONF, 
+    NUCONFORIG, 
+    NUNOTADEV, 
+    NUNOTAORIG, 
+    NUPEDCOMP, 
+    QTDVOL, 
+    STATUS 
+    ) VALUES ( 
+    ${idUsuario}, 
+    NULL, 
+    '${new Date().toISOString().slice(0, 19).replace('T', ' ')}', 
+    ${numeroConferencia}, 
+    NULL, 
+    NULL, 
+    ${numeroNota}, 
+    NULL, 
+    0, 
+    'A' 
+    ); 
+    `);
 
     return { numeroConferencia };
   }
@@ -149,28 +153,6 @@ export class SeparacaoService {
         return { ...data, codigoBarras };
       }),
     );
-
-    const teste = {
-      serviceName: 'DatasetSP.save',
-      requestBody: {
-        entityName: 'CabecalhoConferencia',
-        standAlone: false,
-        fields: ['CODUSUCONF', 'NUCONF', 'QTDVOL', 'STATUS'],
-        records: [
-          {
-            pk: {
-              NUNOTAORIG: 61854,
-            },
-            values: {
-              '0': 0,
-              '1': 6701,
-              '2': 0,
-              '3': 'A',
-            },
-          },
-        ],
-      },
-    };
 
     return response;
   }
